89bbb92158b188304a10c6cce3360f7442e461d1
leolulu_sad_python <348699103@qq.com>
2025-01-22 21:10:27 +0800

    完善了一下Doubao的功能

--- agent/doubao.py
+++ agent/doubao.py
@@ -1,23 +1,49 @@
+from typing import Optional
 from volcenginesdkarkruntime import Ark
 
 
 class Doubao:
-    def __init__(self, api_key, access_point, system_message=None):
+    def __init__(self, api_key, access_point, system_message: Optional[str] = None):
         self.access_point = access_point
         self.system_message = system_message
         self.client = Ark(
             api_key=api_key,
         )
+        self.preserve_messages = []
 
-    def chat(self, message):
-        messages = []
+    def construct_user_message(self, message):
+        return {"role": "user", "content": message}
+
+    def construct_assistant_message(self, message):
+        return {"role": "assistant", "content": message}
+
+    def construct_system_message(self, message):
+        return {"role": "system", "content": message}
+
+    @property
+    def messages(self):
+        return self.messages_system_part + self.preserve_messages
+
+    @property
+    def messages_system_part(self):
         if self.system_message:
-            messages.append({"role": "system", "content": self.system_message})
-        messages.append({"role": "user", "content": message})
+            return [self.construct_system_message(self.system_message)]
+        else:
+            return []
+
+    def chat(self, message, preserve_history=False):
+        self.preserve_messages.append(self.construct_user_message(message))
+
+        if preserve_history:
+            messages = self.messages
+        else:
+            messages = self.messages_system_part + [self.construct_user_message(message)]
 
         completion = self.client.chat.completions.create(
             model=self.access_point,
             messages=messages,
         )
 
-        return completion.choices[0].message.content  # type: ignore
+        response_content = completion.choices[0].message.content
+        self.preserve_messages.append(self.construct_assistant_message(response_content))
+        return response_content
