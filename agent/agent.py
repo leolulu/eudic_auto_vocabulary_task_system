12b20581e73887541d548692fdc97b74c6e89fcd
cesspit <348699103@qq.com>
2025-05-26 11:53:54 +0800

    小结构重构

--- agent/agent.py
+++ agent/agent.py
@@ -10,21 +10,27 @@ from utils.yaml_config_manager import YamlConfigManager
 class Agent:
     def __init__(self) -> None:
         self.config_manager = YamlConfigManager()
+        self.dida = self.get_dida()
+        self.doubao = self.get_doubao()
+        self.eudic = self.get_eudic()
 
-        self.dida = Dida365Agent(
+    def get_dida(self):
+        return Dida365Agent(
             Dida365Api(
                 username=self.config_manager.get_config(DIDA365_USERNAME),
                 password=self.config_manager.get_config(DIDA365_PASSWORD),
             ),
         )
 
+    def get_doubao(self):
         # self.doubao = Doubao(
         #     api_key=self.config_manager.get_config(DOUBAO_API_KEY),
         #     access_point=self.config_manager.get_config(DOUBAO_MODEL_ACCESS_POINT),
         # )
-        self.doubao = DoubaoOnline(endpoint=self.config_manager.get_config(DOUBAO_WEBSERVER_ENDPOINT))
+        return DoubaoOnline(endpoint=self.config_manager.get_config(DOUBAO_WEBSERVER_ENDPOINT))
 
-        self.eudic = Eudic(api_key=self.config_manager.get_config(EUDIC_API_KEY))
+    def get_eudic(self):
+        return Eudic(api_key=self.config_manager.get_config(EUDIC_API_KEY))
 
     def substitute_new_doubao_agent(self):
         # self.doubao = Doubao(
